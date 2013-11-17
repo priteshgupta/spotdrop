5c4ea622fbf4de89a8e99f3999bc44abff2bf1cf
priteshgupta <connect@priteshgupta.com>
2013-11-16 22:13:24 -0500

    ...

--- server/db.php
+++ server/db.php
@@ -11,7 +11,7 @@ $link = mysql_connect('localhost', 'root', 'password');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
-echo 'Connected successfully';
+echo 'Connected successfully\n';
 
 if (!mysql_select_db('spotdrop', $link)) {
     echo 'Could not select database';
@@ -27,17 +27,10 @@ if (!$result) {
     exit;
 }
 
-var_dump(mysql_fetch_assoc($result));
-
 while ($row = mysql_fetch_assoc($result)) {
-    echo $row['foo'];
-
     var_dump($row);
-
 }
-
-echo "\nHello World";
 
 mysql_free_result($result);
 
-//mysql_close($link);
+mysql_close($link);
