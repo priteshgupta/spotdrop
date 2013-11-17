9ed34be7ca25532b43e0e5684ee4a0f1a2a8b18e
Tom P <trpapp@buffalo.edu>
2013-11-17 08:51:39 -0500

    rere-edited zoom level

--- js/functions.js
+++ js/functions.js
@@ -75,7 +75,7 @@ function initialize(position) {
         draggable: true,
         disableDoubleClickZoom: true,     //disable zooming
         scrollwheel: true,
-        zoom: 8,
+        zoom: 12,
         center: latLng,
         mapTypeControl: {mapTypeId: [google.maps.MapTypeId.ROADMAP, 'tehgrayz']},
         mapTypeId: google.maps.MapTypeId.HYBRID //   ROADMAP; SATELLITE; HYBRID; TERRAIN;
